1eaeb60f4036554bcd4eb1a718b783bd6afdc894
zlikun <zlikun-dev@hotmail.com>
2017-12-25 21:53:37 +0800

    ConnectionFactory部分属性标记注释

--- spring-amqp/src/test/java/com/zlikun/learning/AmqpTest.java
+++ spring-amqp/src/test/java/com/zlikun/learning/AmqpTest.java
@@ -1,16 +1,20 @@
 package com.zlikun.learning;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * http://www.rabbitmq.com/tutorials/tutorial-one-java.html
+ *
  * @author zlikun <zlikun-dev@hotmail.com>
  * @date 2017-12-25 18:24
  */
+@Slf4j
 public class AmqpTest {
 
     private final String HOST = "rabbitmq.zlikun.com";
@@ -24,6 +28,19 @@ public class AmqpTest {
         factory.setHost(HOST);
         factory.setUsername("root");
         factory.setPassword("123456");
+
+        // 设置自动重连
+        factory.setAutomaticRecoveryEnabled(true);
+        // 设置每10秒，重试一次
+        factory.setNetworkRecoveryInterval(10_000);
+        // 设置重新声明交换器，队列等信息
+        factory.setTopologyRecoveryEnabled(true);
+
+        // 连接超时时间，单位：毫秒，默认：60秒
+        factory.setConnectionTimeout(15_000);
+        // 通道RPC调用超时时间，单位：毫秒，默认：10分钟
+        factory.setChannelRpcTimeout(120_000);
+
     }
 
     @Test
@@ -32,10 +49,12 @@ public class AmqpTest {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        AMQP.Queue.DeclareOk declareOk = channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        log.info("QUEUE NAME is {}", declareOk.getQueue());
+
         String message = "Hello World!";
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println(" [x] Sent '" + message + "'");
+        log.info("[x] Sent '{}'", message);
 
         channel.close();
         connection.close();
